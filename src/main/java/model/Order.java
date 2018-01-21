59707a24e4884a228187ca0861375957f63d5965
AleksandrKriukov <alexanderkry@gmail.com>
2018-01-21 21:09:59 +0300

    refactored model classes

---
+++ src/main/java/model/Order.java
@@ -0,0 +1,68 @@
+package model;
+
+public class Order {
+
+    private int id;
+    private String name;
+    private float paramMass;
+    private float paramVolume;
+    private String status;
+    private int executor;
+    private int manager;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getParamMass() {
+        return paramMass;
+    }
+
+    public void setParamMass(float paramMass) {
+        this.paramMass = paramMass;
+    }
+
+    public float getParamVolume() {
+        return paramVolume;
+    }
+
+    public void setParamVolume(float paramVolume) {
+        this.paramVolume = paramVolume;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getExecutor() {
+        return executor;
+    }
+
+    public void setExecutor(int executor) {
+        this.executor = executor;
+    }
+
+    public int getManager() {
+        return manager;
+    }
+
+    public void setManager(int manager) {
+        this.manager = manager;
+    }
+}
