ab2c744d4f197e5ffe5005d9be7bb5e6572849aa
Anatolii Syunayev <anatolii_syunaev@epam.com>
2018-01-23 10:48:37 +0300

    dao method renamed

--- src/main/java/dao/RideDao.java
+++ src/main/java/dao/RideDao.java
@@ -6,19 +6,19 @@ import java.util.List;
 public interface RideDao {
 
     //create
-    boolean insert(Ride ride);
+    void add(Ride ride);
 
     //read
-    Ride getById(long id);
+    //Ride getById(long id);
 
     List<Ride> getAll();
 
-    List<Ride> getByStatus(String status);
+    //List<Ride> getByStatus(String status);
 
     //update
-    boolean update(Ride ride);
+    void update(Ride ride);
 
     //delete
-    boolean delete(Ride ride);
+    void delete(Ride ride);
 
 }
