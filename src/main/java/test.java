4405c5d3a204881f9c68525c0dbaa2782d64bf97
Anatolii Syunayev <anatolii_syunaev@epam.com>
2018-01-23 14:52:29 +0300

    role and roledao deleted

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,9 +1,5 @@
-import dao.RoleDao;
-import model.Role;
-
 public class test {
     public static void main(String[] args) {
-        RoleDao roleDao = new RoleDao();
-        roleDao.addRole(new Role("123123"));
+
     }
 }
