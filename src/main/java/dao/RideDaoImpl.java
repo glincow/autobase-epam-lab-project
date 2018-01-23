5013a20e6cd4265f8d728f569f3bf013b65e58ae
Anatolii Syunayev <anatolii_syunaev@epam.com>
2018-01-23 12:31:37 +0300

    ride dao changed

---
+++ src/main/java/dao/RideDaoImpl.java
@@ -0,0 +1,158 @@
+package dao;
+
+import model.Ride;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.dbutils.DbUtils;
+
+public class RideDaoImpl implements RideDao {
+
+    //private Connection connection = DBConnectionPool.getInstance().getConnection();
+
+    @Override
+    public void add (Ride ride) throws DaoException {
+        Connection connection = DBConnectionPool.getInstance().getConnection();
+        PreparedStatement preparedStatement = null;
+
+        String sql = "INSERT INTO Ride (id , name, mass, volume, status, executor_id, manager_id) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, ride.getId());
+            preparedStatement.setString(2, ride.getName());
+            preparedStatement.setFloat(3, ride.getMass());
+            preparedStatement.setFloat(4, ride.getVolume());
+            preparedStatement.setString(5, ride.getStatus());
+            preparedStatement.setString(6, ride.getExecutor().getId());
+            preparedStatement.setString(7, ride.getManager().getId());
+
+            preparedStatement.executeQuery();
+        } catch (SQLException e) {
+            //TODO logging
+            throw new DaoException("SQLexception in add method", e);
+        } finally {
+            /*try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                throw new DaoException("closing connections error in insert method", e);
+            }*/
+            try {
+                DbUtils.closeQuietly(preparedStatement);
+                DbUtils.close(connection);
+            } catch (SQLException e) {
+                //TODO logging
+                throw new DaoException("SQLexception while closing connections error in add method", e);
+            }
+
+        }
+    }
+
+    @Override
+    public List<Ride> getAll() {
+
+        Connection connection = DBConnectionPool.getInstance().getConnection();
+        PreparedStatement preparedStatement = null;
+        ResultSet rs = null;
+        String sql = "SELECT * FROM Ride";
+        List<Ride> list = new ArrayList<>();
+
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+
+            rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                Ride ride = new Ride();
+                ride.setId(rs.getLong("id "));
+                ride.setName(rs.getString("name"));
+                ride.setMass(rs.getFloat("mass"));
+                ride.setVolume(rs.getFloat("volume"));
+                ride.setStatus(rs.getString("status"));
+                ride.setExecutor(rs.getString("executor_id"));
+                ride.setManager(rs.getString("manager_id"));
+                list.add(ride);
+            }
+        } catch (SQLException e) {
+            //TODO logging
+            throw new DaoException("SQLexception in get method", e);
+        } finally {
+            DbUtils.closeQuietly(connection, preparedStatement, rs);
+        }
+
+        return list;
+    }
+
+    @Override
+    public void update(Ride ride) {
+
+        Connection connection = DBConnectionPool.getInstance().getConnection();
+        PreparedStatement preparedStatement = null;
+        String sql = "UPDATE ride SET name = ?, " +
+                "mass = ?, volume = ?, status = ?, executor_id = ?, " +
+                "manager_id = ? WHERE id = ?";
+
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, ride.getName());
+            preparedStatement.setFloat(2, ride.getMass());
+            preparedStatement.setFloat(3, ride.getVolume());
+            preparedStatement.setString(4, ride.getStatus());
+            preparedStatement.setString(5, ride.getExecutor());
+            preparedStatement.setString(6, ride.getManager());
+            preparedStatement.setLong(7, ride.getid());
+
+            preparedStatement.executeQuery();
+        } catch (SQLException e) {
+            //TODO logging
+            throw new DaoException("SQLexception in update method", e);
+        } finally {
+            try {
+                DbUtils.closeQuietly(preparedStatement);
+                DbUtils.close(connection);
+            } catch (SQLException e) {
+                //TODO logging
+                throw new DaoException("SQLException while closing connections in update method", e);
+            }
+        }
+
+    }
+
+    @Override
+    public void delete(Ride ride) {
+
+        Connection connection = DBConnectionPool.getInstance().getConnection();
+        PreparedStatement preparedStatement = null;
+        String sql = "DELETE FROM Ride WHERE id = ?";
+
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1, ride.getId());
+
+            preparedStatement.executeQuery();
+        } catch (SQLException e) {
+            //TODO logging
+            throw new DaoException("SQLexception in delete method", e);
+        } finally {
+            try {
+                DbUtils.closeQuietly(preparedStatement);
+                DbUtils.close(connection);
+            } catch (SQLException e) {
+                //TODO logging
+                throw new DaoException("SQLException while closing connections in delete method", e);
+            }
+        }
+
+    }
+
+}
