130dd28449ecd94c91c29f3dd9f7adbf5e537f87
Anatolii Syunayev <anatolii_syunaev@epam.com>
2018-01-24 13:43:43 +0300

    refactoring

--- src/main/java/dao/RideDaoImpl.java
+++ src/main/java/dao/RideDaoImpl.java
@@ -9,13 +9,29 @@ import java.util.ArrayList;
 import java.util.List;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
-
 import org.apache.commons.dbutils.DbUtils;
 import util.DBConnectionPool;
 
 public class RideDaoImpl implements RideDao {
 
-    final static Logger logger = LogManager.getLogger(RideDaoImpl.class);
+    final static private Logger logger = LogManager.getLogger(RideDaoImpl.class);
+
+    final static private TransportDao transportDao = new TransportDaoImpl();
+
+    final static private UserDao userDao = new UserDaoImpl();
+
+    final static private String SQL_INSERT_RIDE = "INSERT INTO Ride (id , name, mass, volume, status, executor_id, manager_id) " +
+            "VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+    final static private String SQL_SELECT_RIDE_BY_ID = "SELECT * FROM Ride WHERE id = ?";
+
+    final static private String SQL_SELECT_ALL_RIDES = "SELECT * FROM Ride";
+
+    final static private String SQL_UPDATE_RIDE = "UPDATE ride SET name = ?, " +
+            "mass = ?, volume = ?, status = ?, executor_id = ?, " +
+            "manager_id = ? WHERE id = ?";
+
+    final static private String SQL_DELETE_RIDE = "DELETE FROM Ride WHERE id = ?";
 
     private Ride assembleRide (ResultSet rs) throws SQLException {
         Ride ride = new Ride();
@@ -25,8 +41,8 @@ public class RideDaoImpl implements RideDao {
         ride.setMass(rs.getFloat("mass"));
         ride.setVolume(rs.getFloat("volume"));
         ride.setStatus(rs.getString("status"));
-//        ride.setExecutor(new TransportDaoImpl().getBy(rs.getLong("executor_id")));
-//        ride.setManager(new UserDaoImpl().getBy(rs.getLong("manager_id")));
+        ride.setExecutor(transportDao.getBy(rs.getLong("executor_id")));
+        ride.setManager(userDao.getBy(rs.getLong("manager_id")));
 
         return ride;
     }
@@ -34,10 +50,12 @@ public class RideDaoImpl implements RideDao {
     @Override
     public void add (Ride ride) throws DaoException {
 
+        logger.debug("add(Ride ride) started...");
+
         Connection connection = null;
         PreparedStatement preparedStatement = null;
 
-        String sql = "INSERT INTO Ride (name, mass, volume, status) VALUES (?, ?, ?, ?)";
+        String sql = SQL_INSERT_RIDE;
 //        String sql = "INSERT INTO Ride (name, mass, volume, status, executor_id, manager_id) VALUES (?, ?, ?, ?, ?, ?)";
 
         try {
@@ -51,6 +69,13 @@ public class RideDaoImpl implements RideDao {
             preparedStatement.setString(4, ride.getStatus());
 //            preparedStatement.setLong(5, ride.getExecutor().getId());
 //            preparedStatement.setLong(6, ride.getManager().getId());
+            preparedStatement.setLong(1, ride.getId());
+            preparedStatement.setString(2, ride.getName());
+            preparedStatement.setFloat(3, ride.getMass());
+            preparedStatement.setFloat(4, ride.getVolume());
+            preparedStatement.setString(5, ride.getStatus());
+            preparedStatement.setLong(6, ride.getExecutor().getId());
+            preparedStatement.setLong(7, ride.getManager().getId());
 
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
@@ -64,10 +89,13 @@ public class RideDaoImpl implements RideDao {
 
     @Override
     public Ride getById(Long id) {
+
+        logger.debug("Ride getBy(Long id) started...");
+
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         ResultSet rs = null;
-        String sql = "SELECT * FROM Ride WHERE id = ?";
+        String sql = SQL_SELECT_RIDE_BY_ID;
         Ride ride;
 
         try {
@@ -91,10 +119,12 @@ public class RideDaoImpl implements RideDao {
     @Override
     public List<Ride> getAll() throws DaoException {
 
+        logger.debug("List<Ride> getAll started...");
+
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         ResultSet rs = null;
-        String sql = "SELECT * FROM Ride";
+        String sql = SQL_SELECT_ALL_RIDES;
         List<Ride> list = new ArrayList<>();
 
         try {
@@ -121,11 +151,11 @@ public class RideDaoImpl implements RideDao {
     @Override
     public void update(Ride ride) throws DaoException {
 
+        logger.debug("update(Ride ride) started...");
+
         Connection connection = null;
         PreparedStatement preparedStatement = null;
-        String sql = "UPDATE ride SET name = ?, " +
-                "mass = ?, volume = ?, status = ?, executor_id = ?, " +
-                "manager_id = ? WHERE id = ?";
+        String sql = SQL_UPDATE_RIDE;
 
         try {
             connection  = DBConnectionPool.getInstance().getConnection();
@@ -153,9 +183,11 @@ public class RideDaoImpl implements RideDao {
     @Override
     public void delete(Ride ride) throws DaoException {
 
+        logger.debug("delete(Ride ride) started...");
+
         Connection connection = null;
         PreparedStatement preparedStatement = null;
-        String sql = "DELETE FROM Ride WHERE id = ?";
+        String sql = SQL_DELETE_RIDE;
 
         try {
             connection = DBConnectionPool.getInstance().getConnection();
