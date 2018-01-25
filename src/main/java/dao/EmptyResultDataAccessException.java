b0effe484dd3d58c26d3c689555264f4e4b72332
Anatolii Syunayev <anatolii_syunaev@epam.com>
2018-01-25 12:58:15 +0300

    Dao exceptions refactored

---
+++ src/main/java/dao/EmptyResultDataAccessException.java
@@ -0,0 +1,8 @@
+package dao;
+
+public class EmptyResultDataAccessException extends DataAccessException {
+
+    public EmptyResultDataAccessException(String message) {
+        super(message);
+    }
+}
